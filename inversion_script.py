23442188ea58ef64c04946b64d06c5e708802fbd
Oscar Mojica <o_mojical@hotmail.com>
2022-02-06 14:34:57 -0300

    Update inversion_script.py

--- inversion_script.py
+++ inversion_script.py
@@ -3,9 +3,11 @@ inversion script
 """
 from shot_control_inversion import ControlInversion
 
+
 def main():
     control_inv = ControlInversion()
     control_inv.run_inversion()
+
 
 if __name__ == "__main__":
     main()
