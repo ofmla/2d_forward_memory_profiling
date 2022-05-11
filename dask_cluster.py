7b329c280ee64b8f0019723182af1ae0c3f758cf
ofmla <o_mojical@hotmail.com>
2022-05-11 18:30:53 -0300

    broadcast solver/unjitted_op and refactor parallelism strategy

--- dask_cluster.py
+++ dask_cluster.py
@@ -19,7 +19,7 @@ from devito import *
 
 from examples.seismic import AcquisitionGeometry, SeismicModel
 from examples.seismic.acoustic import AcousticWaveSolver
-from examples.seismic import TimeAxis, RickerSource, Receiver
+from examples.seismic import TimeAxis, RickerSource, Receiver, setup_geometry
 
 from utils import segy_write, humanbytes
 
@@ -72,7 +72,7 @@ class DaskCluster:
             cluster.scale(jobs=self.config_values["n_workers"])
 
         # Wait for cluster to start
-        time.sleep(30)
+        time.sleep(10)
         self.client = Client(cluster)
         # initialize tasks dictionary
         self._set_tasks_from_files()
@@ -101,27 +101,70 @@ class DaskCluster:
 
         shot_futures = []
         all_shot_results = []
-        par = self.client.scatter({**self.config_values['solver_params']}, broadcast=True)
+        par = self.client.scatter(self.config_values['solver_params'], broadcast=True)
+
+        model = DaskCluster.get_model(self.config_values['solver_params'])
+        t0 = self.config_values['solver_params']['t0']
+        tn = self.config_values['solver_params']['tn']
+        f0 = self.config_values['solver_params']['f0']
+        space_order = self.config_values['solver_params']['space_order']
+        use_solver = self.config_values['solver_params']['use_solver']
+
+        if use_solver:
+            geometry = setup_geometry(model, tn, f0)
+            op = AcousticWaveSolver(model, geometry, space_order=space_order)
+        else:
+            # Create the forward operator step by step as in tutorial 01
+            dt = model.critical_dt
+            time_range = TimeAxis(start=t0, stop=tn, step=dt)
+            src = RickerSource(name='src', grid=model.grid, f0=f0,
+                               npoint=1, time_range=time_range)
+            dobs = Receiver(name='dobs', grid=model.grid, npoint=model.shape[0],
+                            time_range=time_range)
+
+            # Define the wavefield with the size of the model and the time dimension
+            u = TimeFunction(name="u", grid=model.grid, time_order=2,
+                             space_order=space_order)
+            pde = model.m * u.dt2 - u.laplace + model.damp * u.dt
+            stencil = Eq(u.forward, solve(pde, u.forward))
+            #
+            src_term = src.inject(field=u.forward, expr=src * dt**2 / model.m)
+            rec_term = dobs.interpolate(expr=u.forward)
+            op = Operator([stencil] + src_term + rec_term, subs=model.spacing_map)
+
+        f_solver = self.client.scatter(op, broadcast=True)
 
         shot_master_list = [(lambda d: d.update(id=key) or d)(val)
                             for (key, val) in self.tasks_dict.items()]
-        index = 1
-        batch = []
-        for shot in shot_master_list:
-            batch.append(shot)
-            if index % self.config_values["shot_batch_size"] == 0:
-                shot_futures = self.client.map(func,
-                                               batch, solver_params=par,
-                                               resources={'process': 1})
-                all_shot_results.extend(self.client.gather(shot_futures))
-                batch = []
-                print('Completed shots till {0:3d}'.format(index))
-                index = index + 1
-        if batch:
-            shot_futures = self.client.map(func,
-                                           batch, solver_params=par,
-                                           resources={'process': 1})
-            all_shot_results.extend(self.client.gather(shot_futures))
+
+        # Share work roughly evenly between processes. Original list of shots is break up
+        # into many lists. In other words a list of lists will be divided up among the
+        # processes. This is due to the memory increase seen before with the earlier
+        # approach (lines 108-124 of dask_cluster.py in main branch), where the shots were
+        # divided into batches, and each batch was splitted across processes. The latter
+        # strategy would work very well if the memory was released on successfull
+        # completion of the forward modeling for each shot
+        # By doing this, the 'shot_batch_size' parameter becomes unnecessary
+
+        if self.config_values["use_local_cluster"]:
+            p = self.config_values["n_workers"]
+        else:
+            p = self.config_values["n_workers"]*self.config_values["processes"]
+
+        # Note that we assume that the number of shots is greater than the number
+        # of processes
+
+        c = len(shot_master_list)//p
+        r = len(shot_master_list) % p
+        # How many elements break_list should have
+        break_list = [shot_master_list[i*(c+1):i*(c+1)+c+1] if i < r else
+                      shot_master_list[i*c+r:i*c+r+c] for i in range(0, p)]
+
+        shot_futures = self.client.map(func,
+                                       break_list, solver=f_solver,
+                                       solver_params=par,
+                                       resources={'process': 1})
+        all_shot_results.extend(self.client.gather(shot_futures))
 
         return all_shot_results
 
@@ -131,14 +174,92 @@ class DaskCluster:
         all_shot_results = self.launch_tasks(DaskCluster.gen_shot_in_worker)
 
         if all(all_shot_results):
-            print("Successfully generated %d shots" % (len(all_shot_results)))
+            print("Successfully generated %d shots" % (self.config_values['nshots']))
         else:
             raise Exception("Some error occurred. Please check logs")
 
     @staticmethod
-    def gen_shot_in_worker(shot_dict, solver_params):
+    def gen_shot_in_worker(shot_dict, solver, solver_params):
 
-        dtype = solver_params['dtype']
+        model = DaskCluster.get_model(solver_params)
+        # Get parameters
+        shape = model.shape
+        model_name = solver_params['model_name']
+        use_solver = solver_params['use_solver']
+        space_order = solver_params['space_order']
+        t0 = solver_params['t0']
+        tn = solver_params['tn']
+        f0 = solver_params['f0']
+        dt = solver_params['dt']
+
+        autotune = ('aggressive', 'runtime') if len(shape) == 3 else False
+        if use_solver:
+            # Geometry for current shot
+            src = solver.geometry.src
+            dobs = solver.geometry.rec
+        else:
+            time_range = TimeAxis(start=t0, stop=tn, step=model.critical_dt)
+            src = RickerSource(name='src', grid=model.grid, f0=f0,
+                               npoint=1, time_range=time_range)
+            dobs = Receiver(name='dobs', grid=model.grid, npoint=model.shape[0],
+                            time_range=time_range)
+            op = solver
+
+        # Define the wavefield with the size of the model and the time dimension
+        u = TimeFunction(name="u", grid=model.grid, time_order=2,
+                         space_order=space_order)
+
+        if not type(shot_dict) is list:
+            shot_dict = [shot_dict]
+
+        for d in shot_dict:
+            u.data[:] = 0.
+            src.coordinates.data[:] = np.array(d['Source']).reshape((1, len(shape)))
+            dobs.coordinates.data[:] = np.array(d['Receivers'])
+            if use_solver:
+                solver.forward(src=src, rec=dobs, u=u, autotune=autotune)
+            else:
+                op(time=time_range.num-1, src=src, dobs=dobs, dt=model.critical_dt,
+                   u=u, autotune=autotune)
+
+            print('Shot with time interval of {} ms'.format(model.critical_dt))
+
+            str_shot = str(d['id']).zfill(3)
+            filename = '{}_{}_suheader_{}.segy'.format('shot', str_shot, model_name)
+            filename = solver_params['shotfile_path'] + filename
+            if dt is not None:
+                nsamples = int((tn-t0)/dt + 1)
+                data = dobs.resample(num=nsamples)
+            else:
+                dt = model.critical_dt
+                data = dobs
+            # Save shot in segy format
+            if len(shape) == 3:
+                segy_write(data.data[:], [src_coord[0, 0]],
+                           [src.coordinates.data[0, -1]],
+                           data.coordinates.data[:, 0],
+                           data.coordinates.data[:, -1], dt, filename,
+                           sourceY=[src.coordinates.data[0, 1]],
+                           groupY=data.coordinates.data[:, -1])
+            else:
+                segy_write(data.data[:], [src.coordinates.data[0, 0]],
+                           [src.coordinates.data[0, -1]],
+                           data.coordinates.data[:, 0],
+                           data.coordinates.data[:, -1], dt, filename)
+
+        # del v, solver
+        # clear_cache()
+        # gc.collect()
+
+        return True
+
+    @staticmethod
+    def get_model(par_dict):
+        '''
+        Read physical parameters from hdf5 file and
+        create a Model instance
+        '''
+        dtype = par_dict['dtype']
 
         if dtype == 'float32':
             dtype = np.float32
@@ -148,92 +269,19 @@ class DaskCluster:
             raise ValueError("Invalid dtype")
 
         # Metadata from hdf5 file
-        with h5py.File(solver_params['parfile_path']+'vp.h5', 'r') as f:
+        with h5py.File(par_dict['parfile_path']+'vp.h5', 'r') as f:
             metadata = json.loads(f['metadata'][()])
-
-        origin = metadata['origin']
-        shape = metadata['shape']
-        spacing = metadata['spacing']
-        model_name = solver_params['model_name']
-
-        #
-        vp = np.empty(shape)
-        # Read medium parameters
-        with h5py.File(solver_params['parfile_path']+'vp.h5', 'r') as f:
+            #
+            origin = metadata['origin']
+            shape = metadata['shape']
+            spacing = metadata['spacing']
+            #
+            vp = np.empty(shape)
+            # Read medium parameters
             vp[:] = f['vp'][()]
 
-        # Get src/rec coords
-        src_coord = np.array(shot_dict['Source']).reshape((1, len(shape)))
-        rec_coord = np.array(shot_dict['Receivers'])
+        space_order = par_dict['space_order']
+        nbl = par_dict['nbl']
 
-        space_order = solver_params['space_order']
-        dt = solver_params['dt']
-        nbl = solver_params['nbl']
-        f0 = solver_params['f0']
-        t0 = solver_params['t0']
-        tn = solver_params['tn']
-        use_solver = solver_params['use_solver']
-
-        model = SeismicModel(vp=vp, origin=origin, shape=vp.shape, spacing=spacing,
-                             space_order=space_order, nbl=nbl, bcs="damp", dtype=dtype)
-
-        autotune = ('aggressive', 'runtime') if len(shape) == 3 else False
-        if use_solver:
-            # Geometry for current shot
-            geometry = AcquisitionGeometry(model, rec_coord, src_coord, t0, tn,
-                                           f0=f0, src_type='Ricker')
-            # Set up solver.
-            solver = AcousticWaveSolver(model, geometry, space_order=space_order)
-            dobs = solver.forward(autotune=autotune)[0]
-        else:
-            # Create the forward operator step by step as in tutorial 01
-            dt2 = model.critical_dt
-            time_range = TimeAxis(start=t0, stop=tn, step=dt2)
-            src = RickerSource(name='src', grid=model.grid, f0=f0,
-                               npoint=1, time_range=time_range)
-            src.coordinates.data[:] = src_coord
-            dobs = Receiver(name='dobs', grid=model.grid, npoint=rec_coord.shape[0],
-                            time_range=time_range)
-            dobs.coordinates.data[:] = rec_coord
-
-            # Define the wavefield with the size of the model and the time dimension
-            u = TimeFunction(name="u", grid=model.grid, time_order=2,
-                             space_order=space_order)
-            pde = model.m * u.dt2 - u.laplace + model.damp * u.dt
-            stencil = Eq(u.forward, solve(pde, u.forward))
-            #
-            src_term = src.inject(field=u.forward, expr=src * dt2**2 / model.m)
-            rec_term = dobs.interpolate(expr=u.forward)
-            op = Operator([stencil] + src_term + rec_term, subs=model.spacing_map)
-
-            op(time=time_range.num-1, dt=model.critical_dt, autotune=autotune)
-
-        print('Shot with time interval of {} ms'.format(model.critical_dt))
-
-        str_shot = str(shot_dict['id']).zfill(3)
-        filename = '{}_{}_suheader_{}.segy'.format('shot', str_shot, model_name)
-        filename = solver_params['shotfile_path'] + filename
-        if dt is not None:
-            nsamples = int((tn-t0)/dt + 1)
-            data = dobs.resample(num=nsamples)
-        else:
-            dt = model.critical_dt
-            data = dobs
-        # Save shot in segy format
-        if len(shape) == 3:
-            segy_write(data.data[:], [src_coord[0, 0]],
-                       [src_coord[0, -1]],
-                       data.coordinates.data[:, 0],
-                       data.coordinates.data[:, -1], dt, filename,
-                       sourceY=[src_coord[0, 1]],
-                       groupY=data.coordinates.data[:, -1])
-        else:
-            segy_write(data.data[:], [src_coord[0, 0]],
-                       [src_coord[0, -1]],
-                       data.coordinates.data[:, 0],
-                       data.coordinates.data[:, -1], dt, filename)
-        # del u
-        # clear_cache()
-        # gc.collect()
-
-        return True
+        return SeismicModel(vp=vp, origin=origin, shape=shape, spacing=spacing,
+                            space_order=space_order, nbl=nbl, bcs="damp", dtype=dtype)
